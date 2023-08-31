94bc50b41bf4402700d4c29f272e0243e763b19d
Youssef Shabo <youssefshabo@gmail.com>
2023-08-31 13:35:01 -0500

    snake can eat the fruit and grow now

--- snake/src/components/Game.tsx
+++ snake/src/components/Game.tsx
@@ -7,11 +7,14 @@ import { Coordinate, Direction, GestureEventType } from '../types/types';
 import { View } from 'react-native';
 import Snake from './snake';
 import { checkGameOver } from '../utils/checkGameOver';
+import Food from './Food';
+import { checkEatsFood } from '../utils/checkEatsFood';
+import { randomFoodPosition } from '../utils/randomFoodPosition';
 
 
 const SNAKE_INITIAL_POSITION = [{ x: 5, y: 5}];
 const FOOD_INITIAL_POSITION = { x: 5, y: 20};
-const GAME_BOUNDS = { xMin: 0, xMax: 35, yMin: 0, yMax: 63 };
+const GAME_BOUNDS = { xMin: 1, xMax: 35, yMin: 1, yMax: 72 };
 const MOVE_INTERVAL = 50;
 const SCORE_INCREMENT = 10;
 
@@ -20,9 +23,9 @@ export default function Game():JSX.Element {
     const [direction, setDirection] = useState<Direction>(Direction.Right);
     const [snake, setSnake] = useState<Coordinate[]>(SNAKE_INITIAL_POSITION);
     const [food, setFood] = useState<Coordinate>(FOOD_INITIAL_POSITION);
-    const [score, setScore] = useState<number>(0);
     const [isGameOver, setIsGameOver] = useState<boolean>(false);
     const [isPaused, setIsPaused] = useState<boolean>(false);
+    const [score, setScore] = useState<number>(0);
 
     React.useEffect(() => {
         if (!isGameOver) {
@@ -59,11 +62,14 @@ if (checkGameOver(snakeHead, GAME_BOUNDS)) {
                 break;
         }
 
-        // if eats food
-        //grow snake
-
+    if (checkEatsFood(newHead, food, 2)) {
+        setSnake([newHead, ...snake]);
+        setFood(randomFoodPosition(GAME_BOUNDS.xMax, GAME_BOUNDS.yMax));
+        setScore(score + SCORE_INCREMENT);
+    } else {
         setSnake([newHead, ...snake.slice(0,-1)]);
-    };
+    }
+};
 
     const handleGesture = (event: GestureEventType) => {
         const { translationX, translationY } = event.nativeEvent;
@@ -91,6 +97,7 @@ if (checkGameOver(snakeHead, GAME_BOUNDS)) {
             <SafeAreaView style={styles.container}>
                 <View style={styles.boundaries}>
                     <Snake snake={snake} />
+                    <Food x={food.x} y={food.y} />
                 </View>
             </SafeAreaView>
         </PanGestureHandler>
